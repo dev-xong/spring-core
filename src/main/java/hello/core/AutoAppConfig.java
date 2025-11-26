054d72e5e051ffa0790b26b19eaf8e73f32afb28
xongyuna <123929671+dev-xong@users.noreply.github.com>
2025-11-26 22:22:05 +0900

    excludeFilters, includeFilters 활용

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,13 @@
 package hello.core;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-import java.beans.ConstructorProperties;
-
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
